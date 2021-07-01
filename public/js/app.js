c02666ba981a22f37a44ccfb92fda77fe768a028
Marcello Muscara <marcello@codeministry.de>
2021-07-01 12:49:11 +0200

    Adde simple vuejs method example

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,28 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
-      subheading: 'Und alle Interessent*innen',
-      introduction: 'Mit diesem Formular werden sie ein Teil der wilden Füchse Religion',
-      text1: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
-      text2: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
-      text3: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
+        message: 'Hello Vue!',
+        subheading: 'Und alle Interessent*innen',
+        introduction: 'Mit diesem Formular werden sie ein Teil der wilden Füchse Religion',
+        text1: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
+        text2: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
+        text3: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
     }
-  })
+})
+
+var counter = new Vue({
+    el: '#counter',
+    data: {
+        number: 0
+    },
+    methods: {
+        count: function (event) {
+            this.number = this.number + 1;
+
+            var counterModalElement = document.getElementById('counterModal');
+            var counterModal = new bootstrap.Modal(counterModalElement, {})
+            
+            counterModal.toggle()
+        }
+    }
+})

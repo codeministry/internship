ba8a90e7474c1ffd71d92b545ff6481a6d78ab64
Jan-Luca Neiß <neiss2@icloud.com>
2021-07-01 11:10:28 +0200

    Added variables to template

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      subheading: 'Und alle Interessent*innen',
+      introduction: 'Mit diesem Formular werden sie ein Teil der wilden Füchse Religion',
+      text1: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
+      text2: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
+      text3: 'Lorem Consequat fugiat elit voluptate consequat dolor consectetur magna consequat nisi ut labore',
     }
   })
